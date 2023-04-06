0bd15713ae1ce155d1b1315ad664854c26dce1b5
Lancet <709111609@qq.com>
2023-04-06 14:11:42 +0800

    motion layout base scene done

--- app/src/main/java/cn/lancet/navigation/MotionLayoutActivity.kt
+++ app/src/main/java/cn/lancet/navigation/MotionLayoutActivity.kt
@@ -2,12 +2,15 @@ package cn.lancet.navigation
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import cn.lancet.navigation.databinding.ActivityMotionBinding
 
 class MotionLayoutActivity: AppCompatActivity() {
 
+    private lateinit var mBinding: ActivityMotionBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_motion_layout)
+        mBinding = ActivityMotionBinding.inflate(layoutInflater)
+        setContentView(mBinding.root)
     }
 
 }
