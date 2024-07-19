4fc02b72b10bbf7b6e096705fb09572e4ce5aeaf
Lancet <709111609@qq.com>
2024-07-19 16:14:08 +0800

    uodate version

--- app/src/main/java/cn/lancet/navigation/MainActivity.kt
+++ app/src/main/java/cn/lancet/navigation/MainActivity.kt
@@ -2,21 +2,76 @@ package cn.lancet.navigation
 
 
 import android.content.Intent
+import android.content.IntentSender
 import android.os.Bundle
+import android.util.Log
+import androidx.activity.result.IntentSenderRequest
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.commitNow
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
 import cn.lancet.navigation.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.play.core.appupdate.AppUpdateManager
+import com.google.android.play.core.appupdate.AppUpdateManagerFactory
+import com.google.android.play.core.appupdate.AppUpdateOptions
+import com.google.android.play.core.common.IntentSenderForResultStarter
+import com.google.android.play.core.install.InstallStateUpdatedListener
+import com.google.android.play.core.install.model.AppUpdateType
+import com.google.android.play.core.install.model.InstallStatus
+import com.google.android.play.core.install.model.UpdateAvailability
 import com.gyf.immersionbar.ImmersionBar
 import com.hjq.toast.Toaster
 
 class MainActivity : AppCompatActivity() {
 
+    val UPDATE_REQUEST_CODE = 0x0001
+
     private var mBinding: ActivityMainBinding? = null
 
     private var mNavController: NavController? = null
+
+    private val mAppUpdateManager:AppUpdateManager by lazy {
+        AppUpdateManagerFactory.create(this)
+    }
+
+    private val updateLauncher = registerForActivityResult(
+        ActivityResultContracts.StartIntentSenderForResult()
+    ) { result ->
+        if (result.data == null) return@registerForActivityResult
+        if (result.resultCode == UPDATE_REQUEST_CODE) {
+            Toaster.show("开始更新...")
+            if (result.resultCode != RESULT_OK) {
+                Toaster.show("更新失败")
+            }
+        }
+    }
+
+    private val updateResultStarter =
+        IntentSenderForResultStarter { intent,_,fillInIntent,flagsMask,flagsValues,_,_ ->
+            val request = IntentSenderRequest.Builder(intent)
+                .setFillInIntent(fillInIntent)
+                .setFlags(flagsValues, flagsMask)
+                .build()
+            updateLauncher.launch(request)
+        }
+
+    val listener = InstallStateUpdatedListener { state ->
+        if (state.installStatus() == InstallStatus.DOWNLOADING) {
+            val bytesDownloaded = state.bytesDownloaded()
+            val totalBytesToDownload = state.totalBytesToDownload()
+            Log.d("MainActivity", "Downloaded $bytesDownloaded / $totalBytesToDownload")
+        }
+        if (state.installStatus() == InstallStatus.DOWNLOADED) {
+            Snackbar.make(mBinding!!.root, "New App is ready", Snackbar.LENGTH_INDEFINITE)
+                .setAction("Restart") {
+                    mAppUpdateManager.completeUpdate()
+                }
+                .show()
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,8 +82,15 @@ class MainActivity : AppCompatActivity() {
         ImmersionBar.with(this).init()
         ImmersionBar.with(this).statusBarDarkFont(true).init()
 
+        mAppUpdateManager.registerListener(listener)
+
+        checkUpdate()
+
         mBinding?.fabCreateNotice?.setOnClickListener {
-            Toaster.show("创建新的角色")
+            Snackbar.make(mBinding!!.bottomNavigationView,"创建新的角色",Snackbar.LENGTH_SHORT)
+                .setAction("好的"){
+
+                }.show()
         }
 
         val navHostFragment = NavHostFragment.create(R.navigation.lancet_navigation)
@@ -49,5 +111,42 @@ class MainActivity : AppCompatActivity() {
 
     }
 
+    override fun onResume() {
+        super.onResume()
+        mAppUpdateManager.appUpdateInfo.addOnSuccessListener { appUpdateInfo ->
+            if (appUpdateInfo.updateAvailability() == UpdateAvailability.DEVELOPER_TRIGGERED_UPDATE_IN_PROGRESS) {
+                mAppUpdateManager.startUpdateFlowForResult(
+                    appUpdateInfo,
+                    updateLauncher,
+                    AppUpdateOptions.newBuilder(AppUpdateType.FLEXIBLE).build(),
+                )
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mAppUpdateManager.unregisterListener(listener)
+    }
+
+    private fun checkUpdate(){
+        val appUpdateInfoTask = mAppUpdateManager.appUpdateInfo
+        appUpdateInfoTask.addOnSuccessListener { appUpdateInfo ->
+            if (appUpdateInfo.updateAvailability() == UpdateAvailability.UPDATE_AVAILABLE
+                && appUpdateInfo.isUpdateTypeAllowed(AppUpdateType.FLEXIBLE)) {
+                try {
+                    mAppUpdateManager.startUpdateFlowForResult(
+                        appUpdateInfo,
+                        updateResultStarter,
+                        AppUpdateOptions.newBuilder(AppUpdateType.FLEXIBLE).build(),
+                        UPDATE_REQUEST_CODE
+                    )
+                }catch (e:IntentSender.SendIntentException){
+                    Toaster.show("更新失败")
+                }
+            }
+        }
+
+    }
 
 }
